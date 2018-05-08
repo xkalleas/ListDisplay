11eee9385c72c4b4dc6d1a73f98ef31b80138fb1
xkalleas <xkalleas@gmail.com>
2018-05-08 07:43:43 +0300

    .

--- app/src/main/java/com/example/listdisplay/MainActivity.java
+++ app/src/main/java/com/example/listdisplay/MainActivity.java
@@ -3,9 +3,21 @@ package com.example.listdisplay;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,10 +50,67 @@ public class MainActivity extends AppCompatActivity {
 
         protected String[] doInBackground(Void... voids) {
             // Array of strings...
-            String[] albumArray = {"Android","IPhone","WindowsMobile","Blackberry",
-                    "WebOS","Ubuntu","Windows7","Max OS X"};
+            String[] albumArray = {"Android", "IPhone", "WindowsMobile", "Blackberry",
+                    "WebOS", "Ubuntu", "Windows7", "Max OS X"};
 
+            String albumJsonStr = null;
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+
+            try {
+                final String baseUrl = "http://jsonplaceholder.typicode.com/albums";
+
+                URL url = new URL(baseUrl);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+
+                InputStream inputStream = urlConnection.getInputStream();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+
+                StringBuffer buffer = new StringBuffer();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                try {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                        // But it does make debugging a *lot* easier if you print out the completed
+                        // buffer for debugging.
+                        buffer.append(line + "\n");
+                    }
+                } finally {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                    }
+                }
+
+                albumJsonStr = buffer.toString();
+            } catch (IOException e) {
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+
+            Log.d("XKAL", albumJsonStr);
+
+            ////return getWeatherDataFromJson(forecastJsonStr, numDays);
             return albumArray;
+
+
+            // This will only happen if there was an error getting or parsing the forecast.
+            ////return null;
         }
 
         protected void onPostExecute(String[] albums) {
